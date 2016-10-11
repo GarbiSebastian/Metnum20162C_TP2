126866743cca2a56de90af85375de4c64d6b8213
agallardo <gallardo.agustin@hotmail.com>
2016-10-11 00:57:19 -0300

    creado pequeño script de testing secuencial y archivos de test desde test3.in hasta test11.in. Mañana se suben los resultados

---
+++ TP2/src/testing.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+import sys
+import getopt
+import subprocess
+import time
+import platform
+import os.path
+
+def create(name):
+    try:
+        file=open(name,'w+')
+
+        file.close()
+    except:
+            print("error occured")
+            sys.exit(1)
+
+
+
+def main(argv):
+	for i in xrange(1,12):
+		archivoEntrada = 'tests/test' + str(i) +'.in'
+		archivoSalida = 'results/test' + str(i) +'.out'
+		create(archivoSalida)
+		metodo = str(2)
+		args = ['./tp', archivoEntrada, archivoSalida, metodo]
+		print 'comenzando test' + str(i)
+		process = subprocess.call(args)
+		process.wait()
+		print 'terminado test' + str(i)
+
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
