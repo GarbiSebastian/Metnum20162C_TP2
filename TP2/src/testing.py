b27066acfd89e384c5d61a1cc55f2e185e4f14bb
Agustín Gallardo <agallardo@dc.uba.ar>
2016-11-30 17:21:51 -0300

    Rediseñados tests. Construido generador y ejecutor de tests.

--- TP2/src/testing.py
+++ TP2/src/testing.py
@@ -6,29 +6,66 @@ import subprocess
 import time
 import platform
 import os.path
+import numpy as np
+np.set_printoptions(threshold=np.inf, linewidth=np.inf)
 
-def create(name):
-    try:
-        file=open(name,'w+')
+def createTestingFile(k, param, perc):
+	fname = 'tests/test_'+str(k)+'_'+str(param)+'_'+str(perc)+'.in'
+	with open(fname, 'wr+') as f:
+		file = []
+		f.write('../data/ '+str(k)+' '+str(param)+' '+str(param)+' 10\n')
+		m = perc*42000/100
+		n = 42000 - m
+		for x in range(1,11):
+		 	a = np.array([1]*n + [0]*m)
+			np.random.shuffle(a)
+			f.write(str(a).strip('[]')+'\n')
 
-        file.close()
-    except:
-            print("error occured")
-            sys.exit(1)
+def crearArchivosTesting():
+	for perc in xrange(0, 50, 10):
+		for k in xrange(1, 10, 2):
+			for param in xrange(1, 10):
+				createTestingFile(k, param, perc)
+			for param in xrange(11, 50, 5):
+				createTestingFile(k, param, perc)
 
+def ejecutarArchivosTesting():
+	for perc in xrange(0, 50, 10):
+		for k in xrange(1, 10, 2):
+			for param in xrange(1, 10):
+				for metodo in xrange(0,2):
+				 	archivoEntrada = 'tests/test_'+str(k)+'_'+str(param)+'_'+str(perc)+'.in'
+				 	archivoSalida = 'results/test_'+str(k)+'_'+str(param)+'_'+str(perc)+'.out'
+				 	args = ['./tp', archivoEntrada, archivoSalida, metodo]
+				 	process = subprocess.call(args)
+			for param in xrange(11, 50, 5):
+				for metodo in xrange(0,2):
+					archivoEntrada = 'tests/test_'+str(k)+'_'+str(param)+'_'+str(perc)+'.in'
+				 	archivoSalida = 'results/test_'+str(k)+'_'+str(param)+'_'+str(perc)+'.out'
+				 	args = ['./tp', archivoEntrada, archivoSalida, metodo]
+				 	process = subprocess.call(args)
 
+def crearMiniTest():
+	for perc in xrange(0, 20, 10):
+		for k in xrange(1, 10, 2):
+			for param in xrange(1, 10):
+				createTestingFile(k, param, perc)
+
+def ejecutarMiniTests():
+	for perc in xrange(0, 20, 10):
+		for k in xrange(1, 5, 2):
+			for param in xrange(1, 5):
+				for metodo in xrange(0,2):
+				 	archivoEntrada = 'tests/test_'+str(k)+'_'+str(param)+'_'+str(perc)+'.in'
+				 	archivoSalida = 'results/test_'+str(k)+'_'+str(param)+'_'+str(perc)+'.out'
+				 	args = ['./tp', archivoEntrada, archivoSalida, metodo]
+				 	process = subprocess.call(args)
 
 def main(argv):
-	for i in xrange(1,12):
-		archivoEntrada = 'tests/test' + str(i) +'.in'
-		archivoSalida = 'results/test' + str(i) +'.out'
-		create(archivoSalida)
-		metodo = str(2)
-		args = ['./tp', archivoEntrada, archivoSalida, metodo]
-		print 'comenzando test' + str(i)
-		process = subprocess.call(args)
-		print 'terminado test' + str(i)
-
+	crearMiniTest()
+	ejecutarMiniTests()
+	#crearArchivosTesting()
+	#ejecutarArchivosTesting()
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
